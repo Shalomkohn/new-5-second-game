a8a50961a7e5f39f3976c27b26aa892a0b564a0c
Shalomkohn <sbk5775@gmail.com>
2022-02-27 18:19:46 +0200

    yaaay

--- script.js
+++ script.js
@@ -9,20 +9,89 @@ function mainFunc(){
     var actualGame = document.querySelector('.actualGameSection');
     var pointsScored = document.querySelector('.pointsScored');
     var word = document.querySelector('.word');
-    var timeLeft = document.querySelector('.timeLeft');
+    var timeBarArray = document.querySelectorAll('.timeBarArray')
     var input = document.querySelector('.input');
     var afterGameSection = document.querySelector('.afterGameSection');
     var yourScore  = document.querySelector('.yourScore span');
     var backButton = document.querySelector('.backButton');
-
-    var score = 0;
-    var gameOn = false;
-    var seconds = 4// default to normal
-    var secondsLeft = 0;
-    
-
-
-    var wordArray = [
+    var wordArrayEasy = [
+        'able',
+        'acid',
+        'aged',
+        'also',
+        'area',
+        'army',
+        'away',
+        'baby',
+        'back',
+        'ball',
+        'band',    
+        'bank',    
+        'base',
+        'bath',
+        'bear',
+        'call',
+        'calm',
+        'came',
+        'camp',
+        'card',
+        'care',
+        'case',
+        'cash',
+        'dark',
+        'data',
+        'date',
+        'dawn',
+        'earn',
+        'ease',
+        'east',
+        'easy',
+        'face',
+        'fact',
+        'fail',
+        'fair',
+        'fall',
+        'gave',
+        'gear',
+        'gene',
+        'gift',
+        'hair',
+        'half',
+        'hall',
+        'hand',
+        'hang',
+        'idea',
+        'inch',
+        'into',
+        'iron',
+        'jack',
+        'jane',
+        'jean',
+        'keep',
+        'kent',
+        'kept',
+        'lane',
+        'last',
+        'late',
+        'lead',
+        'left',
+        'mail',
+        'main',
+        'make',
+        'male',
+        'neck',
+        'need',
+        'news',
+        'palm',
+        'park',
+        'rail',
+        'rain',
+        'rank',
+        'side',
+        'sign',
+        'site',
+    ]
+    var wordArrayNormal = [
         'absolute',
         'abstract',
         'academic',
@@ -91,10 +160,105 @@ function mainFunc(){
         'vertical',
         'victoria',
     ];
+    var wordArrayHard = [
+        'susceptible',
+        'proselytize',
+        'expectorate',
+        'fluctuation',
+        'prestigious',
+        'symmetrical',
+        'precalculus',
+        'sponsorship',
+        'stockbroker',
+        'resuscitate',
+        'chocolatier',
+        'directional',
+        'indirection',
+        'particulate',
+        'conditioner',
+        'reanimation',
+        'affirmation',
+        'affirmative',
+        'venturesome',
+        'advancement',
+        'accompanist',
+        'abandonment',
+        'thermometry',
+        'examination',
+        'aspergillum',
+        'teaspoonful',
+        'insinuation',
+        'antinuclear',
+        'impeachment',
+        'peacekeeper',
+        'doublethink',
+        'doublespeak',
+        'thenceforth',
+        'kitchenette',
+        'inauthentic',
+        'calisthenic',
+        'earthenware',
+        'hyphenation',
+        'stegosaurus',
+        'negotiation',
+        'egomaniacal',
+        'egotistical',
+        'conurbation',
+        'wheelbarrow',
+        'chowderhead',
+        'witenagemot',
+        'verisimilar',
+        'cytogenesis',
+        'incongruity',
+        'incongruous',
+        'eschatology',
+        'pastoralism',
+        'culmination',
+        'disorganize',
+        'benediction',
+        'malediction',
+        'concomitant',
+        'commemorate',
+        'survivalist',
+        'survivalism',
+        'clairvoyant',
+        'esotericism',
+        'impulsivity',
+        'concatenate',
+        'acclimatize',
+        'abolishment',
+        'quadrupedal',
+        'eucharistic',
+        'impeachable',
+        'melancholia',
+        'rediscovery',
+        'expectation',
+        'incredulous',
+        'kinesthetic',
+        'kinesthesia',
+        'corporality',
+        'physicality',
+        'contingency',
+        'recognition',
+        'penetration',
+        'consolation',
+        'procreative',
+        'infertility',
+        'ejaculation',
+        'wherewithal',
+        'subsistence',
+        'procreation',
+    ]
+    var arrayOfLevelChosen = wordArrayNormal;//default to normal
+    var score = 0;
+    var gameOn = false;
+    var barNum;
+    
 
-    //only show preGameSection
+    
     actualGame.style.display = 'none';
     afterGameSection.style.display = 'none';
+    //preGameSection.style.display = 'none'
 
     //show high score 
     highScore.innerHTML = localStorage.getItem('High Score');
@@ -108,15 +272,14 @@ function mainFunc(){
             f.style.borderWidth = '1px';
         })
         this.style.borderWidth = '3px';
-        if(this.id === 'easy'){
-            seconds = 5;
-        }else if(this.id === 'normal'){
-            seconds = 4;
+        if(this.id === 'easyButton'){
+            arrayOfLevelChosen = wordArrayEasy;
+        }else if(this.id === 'normalButton'){
+            arrayOfLevelChosen = wordArrayNormal;
         }else{
-            seconds = 3;
+            arrayOfLevelChosen = wordArrayHard;
         }
     }
-
     //Start Game either with button or press enter.
     startButton.forEach(e => {
         e.addEventListener('click', startGame)
@@ -129,20 +292,20 @@ function mainFunc(){
         }
     })
     
-    // countdown ----
+    // countdown time bar----
     setInterval(() => {
-        if(secondsLeft > 0 && gameOn){
-            secondsLeft--;
+        if(barNum > -1 && gameOn){
+            timeBarArray[barNum].style.display = "none";
+            barNum--;
+        }else if (barNum == -1){
+            barNum--
         }
-        timeLeft.innerHTML = secondsLeft;
-    }, 1000);
+    }, 500);
     
-
     function startGame(){
         gameOn = true;
         score = 0;
-        secondsLeft = seconds//depending on dificulty chosen
-        word.innerHTML = wordArray[Math.floor(Math.random() * wordArray.length - 1)];
+        word.innerHTML = arrayOfLevelChosen[Math.floor(Math.random() * arrayOfLevelChosen.length - 1)];
         input.value = '';
         pointsScored.innerHTML = score;
         //swich to actualGameSection
@@ -154,14 +317,14 @@ function mainFunc(){
         if(input.getAttribute("inputmode") == 'none'){
             input.setAttribute("inputmode", "text");
         }
-        
+        barNum = 8;
         //check if game is over or if points are scored.
         setInterval(checkGame, 50);
 
         function checkGame(){
 
             //check for game over
-            if(secondsLeft == 0){
+            if(barNum == -2 && gameOn){
                 gameOn = false;
                 yourScore.innerHTML = score;
                 //check for new high score
@@ -173,15 +336,17 @@ function mainFunc(){
                 actualGame.style.display = 'none';
                 input.setAttribute("inputmode", "none");
                 afterGameSection.style.display = 'grid';
+                timeBarArray.forEach(e => e.style.display = "block")
             }
             
             //check for currect word
             if(input.value.toLowerCase() == word.innerHTML && gameOn){
-                secondsLeft = seconds;
-                word.innerHTML = wordArray[Math.floor(Math.random() * wordArray.length - 1)];
+                barNum = 8
+                word.innerHTML = arrayOfLevelChosen[Math.floor(Math.random() * arrayOfLevelChosen.length - 1)];
                 input.value = '';
                 score++;
                 pointsScored.innerHTML = score;
+                timeBarArray.forEach(e => e.style.display = "block")
             }
 
         }// interval 50
@@ -194,7 +359,6 @@ function mainFunc(){
     function backToHome(){
         afterGameSection.style.display = 'none';
         preGameSection.style.display = 'grid';
-        secondsLeft = seconds;
     }
 
 }
